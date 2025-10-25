2c8ca01dbe28926a2d1a7e34545914f6659f2620
Thiago_19 Notebook <riffothiago19@gmail.com>
2025-10-25 18:00:36 -0300

    Agregando vistas y mejorandolas

--- JS/Inicio.js
+++ JS/Inicio.js
@@ -1,12 +1,8 @@
-const menu = document.getElementById('menu-toggle');
-const sidebar = document.getElementById('sidebar');
-const closeBtn = sidebar.querySelector('.fa-xmark');
 const Recortorios = document.querySelector('.Recordatorios')
 const Editar = document.querySelector('.Hero__header-icon')
 const titulo = document.querySelector('.titulo')
 
 Editar.addEventListener('click', () => {
-  // Crear un input con el texto actual
   const input = document.createElement('input');
   input.type = 'text';
   input.value = titulo.textContent;
@@ -24,17 +20,4 @@ Editar.addEventListener('click', () => {
       input.replaceWith(titulo);
     }
   });
-});
-
-menu.addEventListener('click', () => {
-  if (window.innerWidth <= 900) {
-    sidebar.classList.toggle('show');
-  } else {
-    sidebar.classList.toggle('hide');
-  }
-});
-
-closeBtn.addEventListener('click', () => {
-  sidebar.classList.remove('show');
-  sidebar.classList.add('hide');
 });
